2e0df7717eaad477e96551109303d5b6dbb30d92
Niks4691 <nikspatil1207@gmail.com>
2024-03-17 12:52:07 +0530

    added

---
+++ backend/app.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const cors =require('cors');
+const {db}= require('./db/db')
+const {readdirSync} = require('fs')
+const app = express()
+
+
+require('dotenv').config()
+
+
+
+const PORT = process.env.PORT
+
+//it is used for middlewares
+app.use(express.json())
+app.use(cors())
+//routes
+readdirSync('./routes').map((route) => app.use('/api', require('./routes/' + route)))
+
+const server = () =>{
+
+    db()
+
+    app.listen(PORT,()=>{
+
+        console.log(' server listening on port :',PORT);
+
+    })
+
+   
+}
+server()
